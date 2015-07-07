c0c3decc79c129039f755fc9184add775f541784
Sira Cuervo <fl.siracusa@outlook.com.ar>
2015-07-07 15:08:06 -0300

    Update axxion_mako.mk

--- axxion_mako.mk
+++ axxion_mako.mk
@@ -37,7 +37,7 @@ PRODUCT_MANUFACTURER := LGE
 
 # Kernel inline build
 TARGET_KERNEL_CONFIG := mako_defconfig
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.9
 TARGET_VARIANT_CONFIG := mako_defconfig
 TARGET_SELINUX_CONFIG := mako_defconfig
 
